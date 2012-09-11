825e25e5b9dec8bcaffd04fb3b7b331e2e0979f2
nsf <no.smile.face@gmail.com>
2012-09-11 15:04:44 +0600

    Fix region indent mode highlighting.

--- region_indent_mode.go
+++ region_indent_mode.go
@@ -12,31 +12,21 @@ type region_indent_mode struct {
 func init_region_indent_mode(godit *godit, dir int) region_indent_mode {
 	v := godit.active.leaf
 	r := region_indent_mode{godit: godit}
-	beg := v.cursor
-	end := v.cursor
-	if v.buf.is_mark_set() {
-		end = v.buf.mark
-	}
 
-	if beg.line_num > end.line_num {
-		beg, end = end, beg
-	}
-	beg.boffset = 0
-	end.boffset = len(end.line.data)
-
-	v.set_tags(view_tag{
-		begin_line:   beg.line_num,
-		begin_offset: beg.boffset,
-		end_line:     end.line_num,
-		end_offset:   end.boffset,
-		fg:           termbox.ColorDefault,
-		bg:           termbox.ColorBlue,
-	})
+	beg, end := v.line_region()
 	if dir > 0 {
 		v.on_vcommand(vcommand_indent_region, 0)
 	} else if dir < 0 {
 		v.on_vcommand(vcommand_deindent_region, 0)
 	}
+	v.set_tags(view_tag{
+		beg_line:   beg.line_num,
+		beg_offset: beg.boffset,
+		end_line:   end.line_num,
+		end_offset: end.boffset,
+		fg:         termbox.ColorDefault,
+		bg:         termbox.ColorBlue,
+	})
 	v.dirty = dirty_everything
 	godit.set_status("(Type > or < to indent/deindent respectively)")
 	return r
@@ -51,19 +41,31 @@ func (r region_indent_mode) exit() {
 func (r region_indent_mode) on_key(ev *termbox.Event) {
 	g := r.godit
 	v := g.active.leaf
+	beg, end := v.line_region()
 	if ev.Mod == 0 {
 		switch ev.Ch {
 		case '>':
 			v.on_vcommand(vcommand_indent_region, 0)
 			g.set_status("(Type > or < to indent/deindent respectively)")
-			return
+			goto update_tag
 		case '<':
 			v.on_vcommand(vcommand_deindent_region, 0)
 			g.set_status("(Type > or < to indent/deindent respectively)")
-			return
+			goto update_tag
 		}
 	}
 
 	g.set_overlay_mode(nil)
 	g.on_key(ev)
+	return
+
+update_tag:
+	v.set_tags(view_tag{
+		beg_line:   beg.line_num,
+		beg_offset: beg.boffset,
+		end_line:   end.line_num,
+		end_offset: end.boffset,
+		fg:         termbox.ColorDefault,
+		bg:         termbox.ColorBlue,
+	})
 }
